f61f3283074341135d4dabc4e8e613aae5df3b7c
RizioDev <fabrizioalderete23@gmail.com>
2024-02-06 13:00:02 -0300

    bb

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { FaWhatsapp } from "react-icons/fa";
 import { GrNext } from "react-icons/gr";
